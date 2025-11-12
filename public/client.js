e5524edf2c6b01259cc6df3cec845f0e09f355b9
Wahib Faizi <wahibfaizi@gmail.com>
2025-11-12 15:03:13 +0000

    Refactor client.js

--- public/client.js
+++ public/client.js
@@ -1,14 +1,12 @@
-var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
-
-window.TrelloPowerUp.initialize({
+TrelloPowerUp.initialize({
   'card-back-section': function(t, options){
     return {
       title: 'My Card Back Section',
-      icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+      icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
       content: {
         type: 'iframe',
         url: t.signUrl('./card-back-section.html'),
-        height: 230, // Max height is 1500.
+        height: 320
       }
     };
   }
